59148a5249650301efc88e7d95d28e11cf3ffc0e
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-10-08 12:03:24 -0400

    fixed typos

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "my_key" {
   public_key = file("./modules/ec2/my_key.pub")
 }
 
-resource aws_instance "my_instance" {
+resource "aws_instance" "my_instance" {
     ami = var.ami_id
     instance_type = var.instance_type
     subnet_id = var.subnet_id
@@ -12,7 +12,7 @@ resource aws_instance "my_instance" {
 
     root_block_device {
         volume_size = var.block_storage
-        volume_type = gp3
+        volume_type = "gp3"
     }
 
     tags = {
